29deb2a88cc3813639193ef3ccf9cfaa2bedc1cd
Alvin Chevolleaux <alvin@alvinchevolleaux.com>
2017-02-01 12:14:18 +0000

    Progress with BCMath adapter implementation

--- src/BigInteger/Adapter/BCMathAdapter.php
+++ src/BigInteger/Adapter/BCMathAdapter.php
@@ -5,14 +5,15 @@ declare(strict_types=1);
 namespace Alvtek\OpenIdConnect\BigInteger\Adapter;
 
 use Alvtek\OpenIdConnect\BigInteger\AdapterInterface;
-use Alvtek\OpenIdConnect\BigIntegerInterface;
 use Alvtek\OpenIdConnect\BigInteger\BigIntegerFactory;
+use Alvtek\OpenIdConnect\BigInteger\Exception\OverflowException;
+use Alvtek\OpenIdConnect\BigIntegerInterface;
 
 class BCMathAdapter implements AdapterInterface
 {
     public function toDecimal(BigIntegerInterface $a): string
     {
-        
+        return $this->bcbindec($a->toBytes());
     }
 
     public function toHex(BigIntegerInterface $a): string
@@ -26,7 +27,16 @@ class BCMathAdapter implements AdapterInterface
      */
     public function toInt(BigIntegerInterface $a): int
     {
-        // TODO: Implement toInt() method.
+        $intSize = defined('PHP_INT_SIZE') ? PHP_INT_SIZE : 4;
+    
+        $byteString = $a->toBytes();
+    
+        if (strlen($byteString) > $intSize)  {
+            throw new OverflowException("number is too big to convert to an "
+                . "integer type on this machine.");
+        }
+        
+        return (int) $a->toDecimal();
     }
     
     /**
@@ -36,18 +46,7 @@ class BCMathAdapter implements AdapterInterface
      */
     public function compare(BigIntegerInterface $a, BigIntegerInterface $b): int
     {
-        // TODO: Implement compare() method.
-    }
-    
-    /**
-     * @param BigIntegerInterface $a
-     * @param int $exponent
-     * @param BigIntegerInterface $modulus
-     * @return BigIntegerInterface
-     */
-    public function powerModulus(BigIntegerInterface $a, int $exponent, BigIntegerInterface $modulus): BigIntegerInterface
-    {
-        // TODO: Implement powerModulus() method.
+        return bccomp($a->toDecimal(), $b->toDecimal());
     }
     
     public function decimalToBytes(string $decimal): string
@@ -99,5 +98,71 @@ class BCMathAdapter implements AdapterInterface
     {
         $result = bcpow($a->toDecimal(), $b->toDecimal());
         return BigIntegerFactory::fromDecimal($result);
+    }
+    
+    /**
+     * @param BigIntegerInterface $a
+     * @param int $exponent
+     * @param BigIntegerInterface $modulus
+     * @return BigIntegerInterface
+     */
+    public function powerModulus(BigIntegerInterface $a, int $exponent, BigIntegerInterface $modulus): BigIntegerInterface
+    {
+        $result = bcpowmod($a->toDecimal(), $exponent, $modulus->toDecimal());
+        return BigIntegerFactory::fromDecimal($result);
+    }
+    
+    private function bchexdec(string $hex)
+    {
+        if(strlen($hex) == 1) {
+            return hexdec($hex);
+        }
+        
+        $remain = substr($hex, 0, -1);
+        $last = substr($hex, -1);
+        
+        return bcadd(bcmul(16, $this->bchexdec($remain)), hexdec($last));
+    }
+    
+    private function bcdechex(string $dec)
+    {
+        $last = bcmod($dec, 16);
+        $remain = bcdiv(bcsub($dec, $last), 16);
+        
+        if($remain == 0) {
+            return dechex($last);
+        }
+        
+        return $this->bcdechex($remain).dechex($last);
+    }
+    
+    private function bcbindec(string $bin)
+    {
+        if (strlen($bin) == 1) {
+            return bindec($bin);
+        }
+        
+        $remain = substr($bin, 0, -1);
+        $last = substr($bin, -1);
+        
+        return bcadd(bcmul(256, $this->bcbindec($remain)), bindec($last));
+        
+    }
+    
+    private function bcdecbin(string $decimal)
+    {
+        $last = bcmod($decimal, 256);
+        $remain = bcdiv(bcsub($decimal, $last), 256);
+    
+        if($remain == 0) {
+            return decbin($last);
+        }
+
+        return $this->bcdecbin($remain) . decbin($last);
+    }
+    
+    private function bcbinhex(string $bytes)
+    {
+        
     }
 }
